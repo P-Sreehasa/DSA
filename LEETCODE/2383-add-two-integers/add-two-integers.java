4e51d7c5cab4ed080d14afaadb72470c6b540201
Sreehasa <pendyalasreehasa@gmail.com>
2025-08-23 21:08:36 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (14.42%) - LeetSync

---
+++ LEETCODE/2383-add-two-integers/add-two-integers.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
+
+/*
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;    // Sum of 2 integers: num1 + num2
+    }
+}
+*/
