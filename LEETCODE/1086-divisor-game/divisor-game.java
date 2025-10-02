c762d5400aecfe1afcfd8236a3568583e5bf444c
Sreehasa <pendyalasreehasa@gmail.com>
2025-10-02 23:30:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (50.15%) - LeetSync

---
+++ LEETCODE/1086-divisor-game/divisor-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean divisorGame(int n) {
+       return n % 2 == 0;
+    }
+}
+
+
+/*
+class Solution {
+    public boolean divisorGame(int n) {
+       return n % 2 == 0;    // If n is even: Alice wins, else: Bob wins
+    }
+}
+*/
