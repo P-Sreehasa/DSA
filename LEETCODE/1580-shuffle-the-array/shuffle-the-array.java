a54e92d36eaeb9341bde1e6ebeea3d6baafd14aa
Sreehasa <pendyalasreehasa@gmail.com>
2025-11-14 20:35:01 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (7.94%) - LeetSync

--- LEETCODE/1580-shuffle-the-array/shuffle-the-array.java
+++ LEETCODE/1580-shuffle-the-array/shuffle-the-array.java
@@ -1,14 +1,9 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int arr[] = new int[nums.length];
-        int ind = 0;
+        int arr[] = new int[n * 2];
         for(int i = 0; i < n; i++){
-            int x = nums[i];
-            int y = nums[i + n];
-            arr[ind] = x;
-            ind++;
-            arr[ind] = y;
-            ind++;
+            arr[i * 2] = nums[i];
+            arr[i * 2 + 1] = nums[n + i];
         }
         return arr;
     }
@@ -18,17 +13,12 @@ class Solution {
 /*
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int arr[] = new int[nums.length];    // Array to store the element in [x, y] order
-        int ind = 0;    // Variable to track the array index
-        for(int i = 0; i < n; i++){    // For each value from 0 to n - 1:
-            int x = nums[i];    // Store the values from 0 to n - 1 in x
-            int y = nums[i + n];    // Store the values from n to end of nums array length in y
-            arr[ind] = x;    // Store the x value in the array
-            ind++;    // Increment the index
-            arr[ind] = y;    // Store the y value in the array
-            ind++;    // Increment the index
+        int arr[] = new int[n * 2];    // Create an array of 2n elements
+        for(int i = 0; i < n; i++){    // For each index of the array:
+            arr[i * 2] = nums[i];    // Store the xi-values at even indices
+            arr[i * 2 + 1] = nums[n + i];    // Store the yi-values at odd indices
         }
-        return arr;    // Return the final array in the form of {x, y}
+        return arr;    // Return the final array of the form: [x1,y1,x2,y2,...,xn,yn]
     }
 }
 */
