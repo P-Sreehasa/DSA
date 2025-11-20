cbb23eeb0c50471eea7d42828ef7a30592e6346a
Sreehasa <pendyalasreehasa@gmail.com>
2025-11-20 21:39:45 +0530

    Time: 1 ms (75.65%) | Memory: 42.5 MB (6.84%) - LeetSync

--- LEETCODE/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
+++ LEETCODE/3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -1,14 +1,34 @@
 class Solution {
     public int differenceOfSums(int n, int m) {
-        int yes = 0; 
-        int no = 0;
+        int divisible = 0;
+        int non_divisible = 0;
         for(int i = 1; i <= n; i++){
             if(i % m == 0){
-                yes += i;
+                divisible += i;
             } else{
-                no += i;
+                non_divisible += i;
             }
         }
-        return no - yes;
+        int res = non_divisible - divisible;
+        return res;
     }
 }
+
+
+/*
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int divisible = 0;    // Variable to store the sum of the divisible integers of m
+        int non_divisible = 0;    // Variable to store the sum of the non-divisible integers of m
+        for(int i = 1; i <= n; i++){    // For each value in the range from 1 to n:
+            if(i % m == 0){    // If the value is divisible by m:
+                divisible += i;    // Add it to the divisible variable
+            } else{    // If the value is not divisible by m:
+                non_divisible += i;    // Add it to the non-divisible variable
+            }
+        }
+        int res = non_divisible - divisible;    // Store the difference of the non-divisible and disible values
+        return res;    // Return the result as the final result
+    }
+}
+*/
