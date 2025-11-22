654a20567d136816c78dfd486aace9e36e9b41ae
Sreehasa <pendyalasreehasa@gmail.com>
2025-11-22 21:10:20 +0530

    Time: 1 ms (86.60%) | Memory: 42.9 MB (9.64%) - LeetSync

--- LEETCODE/1635-number-of-good-pairs/number-of-good-pairs.java
+++ LEETCODE/1635-number-of-good-pairs/number-of-good-pairs.java
@@ -1,9 +1,8 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int c = 0;
-        int n = nums.length;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
+        int c = 0; 
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
                 if(nums[i] == nums[j]){
                     c++;
                 }
@@ -12,21 +11,3 @@ class Solution {
         return c;
     }
 }
-
-
-/*
-class Solution {
-    public int numIdenticalPairs(int[] nums) {
-        int c = 0;    // To maintain count of the pairs
-        int n = nums.length;    // Length of the array elements
-        for(int i = 0; i < n; i++){    // Loops from first element to the last
-            for(int j = i + 1; j < n; j++){    // Because of the condition: i < j
-                if(nums[i] == nums[j]){    // To check if elements at 2 indices are same
-                    c++;    // If yes, increment the value of the variable
-                }
-            }
-        }
-        return c;    // Return the no.of times they're equal
-    }
-}
-*/
