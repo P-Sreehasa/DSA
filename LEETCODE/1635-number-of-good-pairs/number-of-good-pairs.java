b2d6606435327738f938a12318d7cbf698cc8f86
Sreehasa <pendyalasreehasa@gmail.com>
2025-11-23 20:11:33 +0530

    Time: 1 ms (86.59%) | Memory: 42.9 MB (10.15%) - LeetSync

--- LEETCODE/1635-number-of-good-pairs/number-of-good-pairs.java
+++ LEETCODE/1635-number-of-good-pairs/number-of-good-pairs.java
@@ -1,15 +1,32 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
-        int c = 0; 
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[j] == nums[i]){
-                    c++;
+        int count = 0;
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                if(nums[i] == nums[j]){
+                    count++;
                 }
             }
         }
-        return c;
+        return count;
     }
 }
 
 
+/*
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;    // Variable to store the count of the no.of good pairs
+        int n = nums.length;    // Calculate the length of the array
+        for(int i = 0; i < n; i++){    // For each index of the array:
+            for(int j = i + 1; j < n; j++){    // For each index of j greater than i:
+                if(nums[i] == nums[j]){    If the values at i and j are equal:
+                    count++;    // Increment the counter variable
+                }
+            }
+        }
+        return count;    // Return the final count of the no.of good pairs
+    }
+}
+*/
