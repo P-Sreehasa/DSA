11a43d50b09b8746dd3ed2c032dafbe7953b87d2
Sreehasa <pendyalasreehasa@gmail.com>
2025-11-24 20:08:37 +0530

    Time: 1 ms (47.89%) | Memory: 42.7 MB (8.88%) - LeetSync

--- LEETCODE/3830-find-closest-person/find-closest-person.java
+++ LEETCODE/3830-find-closest-person/find-closest-person.java
@@ -1,12 +1,28 @@
 class Solution {
     public int findClosest(int x, int y, int z) {
-        int d1 = Math.abs(z - x);
-        int d2 = Math.abs(z - y);
-        if(d1 < d2){
+        int xz = Math.abs(z - x);
+        int yz = Math.abs(z - y);
+        if(xz < yz){
             return 1;
-        } else if(d1 > d2){
+        } else if(xz > yz){
             return 2;
         }
         return 0;
     }
 }
+
+
+/*
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int xz = Math.abs(z - x);    // Calculate the distance from person1 to person3
+        int yz = Math.abs(z - y);    // Calculate the distance from person2 to person3
+        if(xz < yz){    // If person1 arrives first:
+            return 1;    // Return 1
+        } else if(xz > yz){    // If person2 arrives first:
+            return 2;    // Return 2
+        }
+        return 0;    // If both person1 and person2 arrive at the same time, return 0
+    }
+}
+*/
