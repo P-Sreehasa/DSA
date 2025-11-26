1b81f07671ffa50d4d2aef8e38916e633db3e6f1
Sreehasa <pendyalasreehasa@gmail.com>
2025-11-26 21:34:22 +0530

    Time: 5 ms (43.68%) | Memory: 44 MB (12.91%) - LeetSync

--- LEETCODE/3567-convert-date-to-binary/convert-date-to-binary.java
+++ LEETCODE/3567-convert-date-to-binary/convert-date-to-binary.java
@@ -1,29 +1,22 @@
 class Solution {
     public String convertDateToBinary(String date) {
         String dt[] = date.split("-");
-        int y = Integer.parseInt(dt[0]);
-        int m = Integer.parseInt(dt[1]);
-        int d = Integer.parseInt(dt[2]);
-        String year = Integer.toBinaryString(y);  
-        String month = Integer.toBinaryString(m);
-        String day = Integer.toBinaryString(d);  
-        return year + "-" + month + "-" + day;        
+        String year = Integer.toBinaryString(Integer.parseInt(dt[0]));
+        String month = Integer.toBinaryString(Integer.parseInt(dt[1]));
+        String day = Integer.toBinaryString(Integer.parseInt(dt[2]));
+        return year + "-" + month + "-" + day;
     }
 }
-
 
 
 /*
 class Solution {
     public String convertDateToBinary(String date) {
-        String dt[] = date.split("-");    // Array to store the splitted date
-        int y = Integer.parseInt(dt[0]);    // Store the first part as year
-        int m = Integer.parseInt(dt[1]);    // Store the second part as month
-        int d = Integer.parseInt(dt[2]);    // Store the third part as day
-        String year = Integer.toBinaryString(y);    // Convert the integer year into binary
-        String month = Integer.toBinaryString(m);    // Convert the integer month into binary
-        String day = Integer.toBinaryString(d);    // Convert the integer day into binary
-        return year + "-" + month + "-" + day;    // Return the final date         
+        String dt[] = date.split("-");    // Split the date as year, month and day
+        String year = Integer.toBinaryString(Integer.parseInt(dt[0]));    // Store the year as its binary form
+        String month = Integer.toBinaryString(Integer.parseInt(dt[1]));    // Store the month as its binary form
+        String day = Integer.toBinaryString(Integer.parseInt(dt[2]));    // Store the day as its binary form
+        return year + "-" + month + "-" + day;    // Return the final binary date
     }
 }
 */
