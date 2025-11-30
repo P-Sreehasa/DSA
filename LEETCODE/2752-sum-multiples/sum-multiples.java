191406095b00e2bafcffd115c076f0d251eebad9
Sreehasa <pendyalasreehasa@gmail.com>
2025-11-30 18:49:59 +0530

    Time: 3 ms (71.76%) | Memory: 42.6 MB (23.00%) - LeetSync

--- LEETCODE/2752-sum-multiples/sum-multiples.java
+++ LEETCODE/2752-sum-multiples/sum-multiples.java
@@ -3,24 +3,9 @@ class Solution {
         int s = 0;
         for(int i = 1; i <= n; i++){
             if((i % 3 == 0) || (i % 5 == 0) || (i % 7 == 0)){
-                s += i;
+                s = s + i;
             }
         }
         return s;
     }
 }
-
-
-/*
-class Solution {
-    public int sumOfMultiples(int n) {
-        int s = 0;    // Variable to keep track of the sum
-        for(int i = 1; i <= n; i++){    // For each value in the range from 1 to n:
-            if((i % 3 == 0) || (i % 5 == 0) || (i % 7 == 0)){    // If the value is divisible by 3 or 5 or 7:
-                s += i;    // Add it to sum
-            }
-        }
-        return s;    // Return the sum of those elements
-    }
-}
-*/
