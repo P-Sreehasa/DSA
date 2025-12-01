26daf4398b98b2cb7cd4f3dafa1755c0aa23ec11
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-01 20:45:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (5.58%) - LeetSync

--- LEETCODE/2491-smallest-even-multiple/smallest-even-multiple.java
+++ LEETCODE/2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,22 +1,8 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        if(n % 2 == 0){
-            return n;
-        }else{
-            return 2 * n;
+        if(n % 2 != 0){
+            return n * 2;
         }
+        return n;
     }
 }
-
-
-/*
-class Solution {
-    public int smallestEvenMultiple(int n) {
-        if(n % 2 == 0){    // Check if the number is even
-            return n;    // If yes, return the number, since it's already divisible by 2;
-        }else{    
-            return 2 * n;    // Since the number is odd, return the smallest number divisible by both 2 and n (LCM of the number and 2)
-        }
-    }
-}
-*/
