fd1a3bd34d8d8866336f83069b5e82147f2ff83b
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-02 20:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (19.90%) - LeetSync

--- LEETCODE/2491-smallest-even-multiple/smallest-even-multiple.java
+++ LEETCODE/2491-smallest-even-multiple/smallest-even-multiple.java
@@ -6,3 +6,15 @@ class Solution {
         return n;
     }
 }
+
+
+/*
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        if(n % 2 != 0){    // If n is Odd:
+                return n * 2;    // Multiply it by 2
+        }
+        return n;    // If n is Even, return that number itself
+    }
+}
+*/
