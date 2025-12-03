1e442e3e00cf07c96afd4146a125781f623ac90c
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-03 20:21:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (6.46%) - LeetSync

--- LEETCODE/1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.java
+++ LEETCODE/1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -1,13 +1,30 @@
 class Solution {
     public int subtractProductAndSum(int n) {
-        int s = 0; 
-        int p = 1;
+        int sum = 0; 
+        int product = 1;
         while(n > 0){
-            int rem = n % 10;
-            s = s + rem;
-            p = p * rem;
+            int ld = n % 10;
+            sum = sum + ld;
+            product = product * ld;
             n /= 10;
         }
-        return p - s;
+        return product - sum;
     }
 }
+
+
+/*
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int sum = 0;    // Variable to store the sum
+        int product = 1;    // Variable to store the product
+        while(n > 0){    // As long as the number is greater than 0:
+            int ld = n % 10;    // Extract the last digit
+            sum = sum + ld;    // Add it to the sum
+            product = product * ld;    // Multiply it with the product
+            n /= 10;    // Move to the next digit
+        }
+        return product - sum;    // The final result is the difference of product and sum
+    }
+}
+*/
