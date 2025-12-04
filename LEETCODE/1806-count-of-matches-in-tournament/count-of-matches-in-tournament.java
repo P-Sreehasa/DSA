06c4cc92562ce40768f7129b18654a9a4ca950f6
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-04 17:53:11 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (15.65%) - LeetSync

--- LEETCODE/1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
+++ LEETCODE/1806-count-of-matches-in-tournament/count-of-matches-in-tournament.java
@@ -1,35 +1,5 @@
 class Solution {
     public int numberOfMatches(int n) {
-        int c = 0;
-        while(n > 1) {
-            if(n % 2 == 0) {
-                c += n / 2;
-                n = n / 2;
-            }else{
-                c += (n - 1) / 2;
-                n = (n - 1) / 2 + 1;
-            }
-        }
-        return c;
+        return n - 1;
     }
 }
-
-
-
-/*
-class Solution {
-    public int numberOfMatches(int n) {
-        int c = 0;    // To keep count of the total no.of matches
-        while(n > 1) {
-            if(n % 2 == 0) {    // If no.of teams are even:
-                c += n / 2;    // As per given: each team gets paired with another team, so total of n / 2 teams
-                n = n / 2;    // n / 2 teams advance to the next round.
-            }else{    // If no.of teams are odd:
-                c += (n - 1) / 2;    //  One team randomly advances in the tournament, so total of (n - 1) / 2 matches
-                n = (n - 1) / 2 + 1;    // The teams that moved forward are: (n - 1) / 2 + 1
-            }
-        }
-        return c;
-    }
-}
-*/
