d540ae9d33b5bf83fbaad67fdf5e55f6ccfe9ee1
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-08 21:04:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (45.46%) - LeetSync

--- LEETCODE/2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
+++ LEETCODE/2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
@@ -1,15 +1,34 @@
 class Solution {
     public int countDigits(int num) {
-        int o = num;
-        int rem = 0;
-        int c = 0;
+        int temp = num;
+        int last_digit = 0;
+        int count = 0;
         while(num > 0){
-            rem = num % 10;
-            if(o % rem == 0){
-                c++;
+            last_digit = num % 10;
+            if(temp % last_digit == 0){
+                count++;
             }
             num /= 10;
         }
-        return c;
+        return count;
     }
 }
+
+
+/*
+class Solution {
+    public int countDigits(int num) {
+        int temp = num;    // Store the original number in a temporary variable
+        int last_digit = 0;    // Variable to store the last digit of the number
+        int count = 0;    // Variable to track the count of digits
+        while(num > 0){    // As long as the number is greater than zero:
+            last_digit = num % 10;    // Extract the last digit
+            if(temp % last_digit == 0){    // If the original number is divisible by the last digit:
+                count++;    // Increment the counter variable
+            }
+            num /= 10;    // Move to the next digit
+        }
+        return count;    // Return the final count
+    }
+}
+*/
