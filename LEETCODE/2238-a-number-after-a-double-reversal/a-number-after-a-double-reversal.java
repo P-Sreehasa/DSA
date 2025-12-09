149cecb81b53c4cbc203179723a755279ad13fdc
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-09 20:43:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (15.02%) - LeetSync

--- LEETCODE/2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
+++ LEETCODE/2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -1,38 +1,17 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        int n1 = reverse(num);
-        int n2 = reverse(n1);
-        return n2 == num;
-    }
-    
-    public static int reverse(int n){
-        int rev = 0;
-        while(n > 0){
-            int d = n % 10;
-            rev = (rev * 10) + d;
-            n /= 10;
+        int o1 = num;
+        int r1 = 0;
+        int r2 = 0;
+        while(num > 0){
+            r1 = (r1 * 10) + (num % 10); 
+            num /= 10;
         }
-        return rev;
+        int o2 = r1;
+        while(o2 > 0){
+            r2 = (r2 * 10) + (o2 % 10);
+            o2 /= 10;
+        }
+        return r2 == o1;
     }
 }
-
-
-/*
-class Solution {
-    public boolean isSameAfterReversals(int num) {
-        int n1 = reverse(num);    // First reversal
-        int n2 = reverse(n1);    // Second reversal
-        return n2 == num;    // If the reversed number is same as the original, return true, else false
-    }
-
-    public static int reverse(int n){    // Function for reversing a number
-        int rev = 0;    // Variable to store the reversed number
-        while(n > 0){    // For each digit:
-            int d = n % 10;    // Get the last digit
-            rev = (rev * 10) + d;    // Append it to the reversed number
-            n /= 10;    // Remove the last digit
-        }
-        return rev;    // Return the final reversed number
-    }
-}
-*/
