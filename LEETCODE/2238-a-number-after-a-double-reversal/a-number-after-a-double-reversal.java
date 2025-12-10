e7d4c5b67ec4a8c0ec16fba108b91f032089bc79
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-10 20:46:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (27.31%) - LeetSync

--- LEETCODE/2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
+++ LEETCODE/2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -10,3 +10,19 @@ class Solution {
         return true;
     }
 }
+
+
+/*
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        if(num == 0){    // If the number is equal to zero:
+            return true;    // Return true (since, reversing it is still equal to 0)
+        }
+        int rev = num % 10;    // Variable to store the last digit of the number
+        if(rev == 0){    // If the number has 0's at the end:
+            return false;    // Return false (since, reversing it once won't get the original number)
+        }
+        return true;    // If the number does not have 0's at the end, return true (since, reversing such number gives the original number)
+    }
+}
+*/
