cf1b77f5367862fcb0d0f511b6a4c461b7aed356
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-12 18:25:42 +0530

    Time: 3 ms (15.12%) | Memory: 42.5 MB (9.78%) - LeetSync

--- LEETCODE/1448-maximum-69-number/maximum-69-number.java
+++ LEETCODE/1448-maximum-69-number/maximum-69-number.java
@@ -10,3 +10,19 @@ class Solution {
         return Integer.parseInt(s);
     }
 }
+
+
+/*
+class Solution {
+    public int maximum69Number (int num) {
+        String s = String.valueOf(num);    // Store the string equivalent value of number in a variable
+        for(int i = 0; i < s.length(); i++){    // For each value in the length of the string:
+            if(s.charAt(i) == '6'){    // If the current value is 6:
+                s = s.substring(0, i) + '9' + s.substring(i + 1);    // Change it to 9
+                break;    // Terminate the loop
+            }
+        }
+        return Integer.parseInt(s);    // Return the integer value
+    }
+}
+*/
