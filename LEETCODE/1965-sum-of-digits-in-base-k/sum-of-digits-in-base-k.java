6e25a97374865bb9868b3eeea10c23d79c7d3a70
Sreehasa <pendyalasreehasa@gmail.com>
2025-12-17 17:44:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (57.02%) - LeetSync

--- LEETCODE/1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.java
+++ LEETCODE/1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.java
@@ -2,10 +2,25 @@ class Solution {
     public int sumBase(int n, int k) {
         int sum = 0;
         while(n > 0){
-            int rem = n % k;
-            sum = sum + rem;
+            int remainder = n % k;
+            sum += remainder;
             n /= k;
         }
         return sum;
     }
 }
+
+
+/*
+class Solution {
+    public int sumBase(int n, int k) {
+        int sum = 0;    // Variable to store the sum
+        while(n > 0){    // As long as the number is greater than zero:
+            int remainder = n % k;    // Get the remainder of n and k
+            sum += remainder;    // Add it to the sum
+            n /= k;    // Move to the next digit
+        }
+        return sum;    // Return the final sum of the digits
+    }
+}
+*/
