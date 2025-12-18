5ce0a0cd0ef439d144150e3eb41777ff0d27b689
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-12-18 20:25:18 +0530

    Time: 13 ms (59.85%) | Memory: 42.2 MB (41.05%) - LeetSync

--- LEETCODE/2037-count-square-sum-triples/count-square-sum-triples.java
+++ LEETCODE/2037-count-square-sum-triples/count-square-sum-triples.java
@@ -1,36 +1,15 @@
 class Solution {
     public int countTriples(int n) {
-        int a, b, c;
         int cnt = 0;
-        for(a = 1; a <= n; a++){
-            for(b = 1; b <= n; b++){
-                int s = (a * a) + (b * b);
-                c = (int) Math.sqrt(s);
-                if((c * c) == s && c <= n){
+        for(int a = 1; a <= n; a++){
+            for(int b = 1; b <= n; b++){
+                int c_s = (a * a) + (b * b);
+                int c = (int)Math.sqrt(c_s);
+                if(c * c == c_s && c <= n){
                     cnt++;
                 }
             }
-        }
+        }   
         return cnt;
     }
 }
-
-
-/*
-class Solution {
-    public int countTriples(int n) {
-        int a, b, c;    // Variables to track the square triples
-        int cnt = 0;    // Counter variable to track the no.of square triples
-        for(a = 1; a <= n; a++){    // For each value in the range of 1 and n:
-            for(b = 1; b <= n; b++){
-                int s = (a * a) + (b * b);    // Calculate a-square and b-square and store in s
-                c = (int) Math.sqrt(s);    // Calclate the square-root of s and store it in c:
-                if((c * c) == s && c <= n){    // If c-square is a perfect-square:
-                    cnt++;    // Increment the counter variable
-                }
-            }
-        }
-        return cnt;    // Return the final count
-    }
-}
-*/
