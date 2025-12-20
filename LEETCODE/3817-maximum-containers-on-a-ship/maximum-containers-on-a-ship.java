2f7c4535f12de4bc75a75a825bec43eef52edabb
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-12-20 20:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (33.70%) - LeetSync

--- LEETCODE/3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.java
+++ LEETCODE/3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.java
@@ -8,3 +8,17 @@ class Solution {
         return Math.min(max, c);
     }
 }
+
+
+/*
+class Solution {
+    public int maxContainers(int n, int w, int maxWeight) {
+        int max = maxWeight / w;    // Calculate the maximum no.of containers by weight
+        if(max <= n){    // As long as the maximum no.of containers is less than or equal to n:
+            return max;    // Return it as the maximum
+        }
+        int c = n * n;    // No.of cells is calculated by n * n
+        return Math.min(max, c);    // Return the minimum of both
+    }
+}
+*/
