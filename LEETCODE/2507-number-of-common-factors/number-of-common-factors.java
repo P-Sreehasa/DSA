5173eeace14c69558cad53757ceb62e627dd6967
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-12-21 19:53:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (11.53%) - LeetSync

--- LEETCODE/2507-number-of-common-factors/number-of-common-factors.java
+++ LEETCODE/2507-number-of-common-factors/number-of-common-factors.java
@@ -1,38 +1,12 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        while(b != 0){
-            int t = b;
-            b = a % b;
-            a = t;
-        }
-        int gcd = a;
         int c = 0;
-        for(int i = 1; i <= gcd; i++){
-            if(gcd % i == 0){
+        int m = Math.min(a, b);
+        for(int i = 1; i <= m; i++){
+            if(a % i == 0 && b % i == 0){
                 c++;
             }
         }
         return c;
     }
 }
-
-
-/*
-class Solution {
-    public int commonFactors(int a, int b) {
-        while(b != 0){    // As long as the value of b is not equal to 0:
-            int t = b;    // Temporary variable to store the value of b
-            b = a % b;    // Store the remainder (when a is divided by b) in b
-            a = t;    // Store b's actual value in a
-        }
-        int gcd = a;    // Variable to store the gcd value of a and b
-        int c = 0;    // Variable to mainatin count of common factors
-        for(int i = 1; i <= gcd; i++){    // As long as teh number is in the range of 1 and gcd:
-            if(gcd % i == 0){    // If the number divides gcd exactly:
-                c++;    // Increment the counter variable
-            }
-        }
-        return c;    // Return the count (total no.of common factors)
-    }
-}
-*/
