06accfbfbfe47610501bae47a647f64128aae404
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-12-22 20:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.14%) - LeetSync

--- LEETCODE/2507-number-of-common-factors/number-of-common-factors.java
+++ LEETCODE/2507-number-of-common-factors/number-of-common-factors.java
@@ -1,12 +1,28 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int c = 0;
-        int m = Math.min(a, b);
-        for(int i = 1; i <= m; i++){
-            if(a % i == 0 && b % i == 0){
-                c++;
+        int count = 0;
+        int minimum = Math.min(a, b);
+        for(int i = 1; i <= minimum; i++){
+            if((a % i == 0) && (b % i == 0)){
+                count++;
             }
         }
-        return c;
+        return count;
     }
 }
+
+
+/*
+class Solution {
+    public int commonFactors(int a, int b) {
+        int count = 0;    // Variable to count the no.of common factors
+        int minimum = Math.min(a, b);    // Find the minimum value between a and b
+        for(int i = 1; i <= minimum; i++){    // For each value in the range of 1 to minimum:
+            if((a % i == 0) && (b % i == 0)){    // If the current value is divisible by both a and b:
+                count++;    // Increment the counter variable
+            }
+        }
+        return count;    // Return the final count of the no.of common factors of a and b
+    }
+}
+*/
