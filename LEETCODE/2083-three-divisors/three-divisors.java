d13197e088f34d57b2dc92e50bbc3e02321545a9
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-12-23 20:03:01 +0530

    Time: 1 ms (76.17%) | Memory: 42.5 MB (9.69%) - LeetSync

--- LEETCODE/2083-three-divisors/three-divisors.java
+++ LEETCODE/2083-three-divisors/three-divisors.java
@@ -1,38 +1,14 @@
 class Solution {
     public boolean isThree(int n) {
-        for(int i = 2; i * i <= n; i++){
-            if(i * i == n){
-                boolean y = true;
-                for(int j = 2; j * j <= i; j++){
-                    if(i % j == 0){
-                        y = false;
-                        break;
-                    }
-                }
-                return y;
+        int c = 0;
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0){
+                c++;
             }
         }
-        return false;
-    }
-}
-
-
-/*
-class Solution {
-    public boolean isThree(int n) {
-        for(int i = 2; i * i <= n; i++){    // For every value in the range of 2 to sqrt.of n:
-            if(i * i == n){    // If i is the sqrt.of n:
-                boolean y = true;    // Variable to track if the number is prime
-                for(int j = 2; j * j <= i; j++){    // For every value in the range of 2 to sqrt.of n:
-                    if(i % j == 0){    // If i is not prime:
-                        y = false;    // Return false
-                        break;    // Terminate the loop
-                    }
-                }
-                return y;    // Return if the value is prime or not
-            }
+        if(c != 3){
+            return false;
         }
-        return false;    // If n is not a perfect square, return false
+        return true;
     }
 }
-*/
