059a35ee009e0f6727f83705dcd5cecd7e2273ae
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-12-25 20:53:59 +0530

    Time: 3 ms (61.80%) | Memory: 47.3 MB (15.26%) - LeetSync

--- LEETCODE/944-smallest-range-i/smallest-range-i.java
+++ LEETCODE/944-smallest-range-i/smallest-range-i.java
@@ -1,30 +1,12 @@
 class Solution {
     public int smallestRangeI(int[] nums, int k) {
-        Arrays.sort(nums);
         int n = nums.length;
-        int a = nums[n - 1] - nums[0];
-        if(a <= 2 * k){
-            return 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
         }
-        int minn = nums[0] + k;
-        int maxx = nums[n - 1] - k;
-        return maxx - minn;
+        return Math.max(0, max - min - 2 * k);
     }
 }
-
-
-/*
-class Solution {
-    public int smallestRangeI(int[] nums, int k) {
-        Arrays.sort(nums);    // Sort the array to find the minimum and maximum elements
-        int n = nums.length;    // Calculate th length of the array
-        int a = nums[n - 1] - nums[0];    // Store the difference between the largest value and the smallest value in the variable "a"
-        if(a <= 2 * k){    // If the difference is is less than or equal to 2 * k: 
-            return 0;    // Make the difference 0
-        }
-        int minn = nums[0] + k;    // Update the minimum by adding k to the smallest value
-        int maxx = nums[n - 1] - k;    // Update the maximum by subtracting k from the largest value
-        return maxx - minn;    // Return the final difference between the maximum and minimum values
-    }
-}
-*/
