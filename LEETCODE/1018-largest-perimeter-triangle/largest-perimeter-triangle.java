f467f384f2da2af9ff9b17f9101195aaf1597b4a
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-12-28 23:25:49 +0530

    Time: 12 ms (29.71%) | Memory: 48 MB (12.48%) - LeetSync

--- LEETCODE/1018-largest-perimeter-triangle/largest-perimeter-triangle.java
+++ LEETCODE/1018-largest-perimeter-triangle/largest-perimeter-triangle.java
@@ -13,3 +13,22 @@ class Solution {
         return 0;
     }
 }
+
+
+/*
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);    // Sort the array
+        int n = nums.length;    // Calculate the array size
+        for(int i = n - 1; i >= 2; i--){    // For each index of the array from the largest element:
+            int a = nums[i];    // Store the largest element in a
+            int b = nums[i - 1];    // Store the second largest element in b
+            int c = nums[i - 2];    // Store the third largest element in c
+            if(c + b > a){    // If the sum of 2 sides is greater then the 3rd side:
+                return a + b + c;    // Return the sum
+            }
+        }
+        return 0;    // If the sum of 2 sides is not greater then the 3rd side, return 0
+    }
+}
+*/
