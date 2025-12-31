8dbea567c0fdc30382671fe63ce22ce51a8326bb
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2025-12-31 20:44:23 +0530

    Time: 1 ms (89.07%) | Memory: 43.7 MB (14.76%) - LeetSync

--- LEETCODE/171-excel-sheet-column-number/excel-sheet-column-number.java
+++ LEETCODE/171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -1,10 +1,25 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
-        int n = 0;
+        int num = 0;
         for(int i = 0; i < columnTitle.length(); i++){
-            int v = columnTitle.charAt(i) - 'A' + 1;
-            n = n * 26 + v;
+            int value = columnTitle.charAt(i) - 'A' + 1;
+            num = num * 26 + value;
         }
-        return n;
+        return num;
     }
 }
+
+
+
+/*
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int num = 0;    // Variable to store the column number
+        for(int i = 0; i < columnTitle.length(); i++){    // For each character of the input string:
+            int value = columnTitle.charAt(i) - 'A' + 1;    // Store the numerical column value of the current character
+            num = num * 26 + value;    // Multiply the current num value by 26 and add the current character value
+        }
+        return num;    // Return the final column number for the character(s)
+    }
+}
+*/
