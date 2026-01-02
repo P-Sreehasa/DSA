f750e6972d01878a8f94a4769acbec6e756b5b80
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-02 23:36:57 +0530

    Time: 1 ms (99.41%) | Memory: 47.3 MB (13.45%) - LeetSync

--- LEETCODE/2048-build-array-from-permutation/build-array-from-permutation.java
+++ LEETCODE/2048-build-array-from-permutation/build-array-from-permutation.java
@@ -8,3 +8,17 @@ class Solution {
         return ans;
     }
 }
+
+
+/*
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;    // Calculate the array size
+        int ans[] = new int[n];    // Create a new array
+        for(int i = 0; i < n; i++){    // For each index of the original array:
+            ans[i] = nums[nums[i]];    // Store the  value at nums[i] in the new array
+        }
+        return ans;    // Return the nw array
+    }
+}
+*/
