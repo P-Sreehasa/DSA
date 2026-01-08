311fe167e941390c8843f246a4bfca148fefdce6
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-08 20:11:40 +0530

    Time: 1 ms (88.46%) | Memory: 42.5 MB (52.30%) - LeetSync

--- LEETCODE/342-power-of-four/power-of-four.java
+++ LEETCODE/342-power-of-four/power-of-four.java
@@ -9,3 +9,18 @@ class Solution {
         return n == 1;
     }
 }
+
+
+/*
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n <= 0){    // If the number is not greater than 0:
+            return false;    // Return false
+        }
+        while(n % 4 == 0){    // As long as n is a multiple of 4:
+            n /= 4;    // Divide n by 4 to reduce it
+        }
+        return n == 1;    // Return true is n is a power of 4, else false
+    }
+}
+*/
