7928858aa7828d932cd954b2e8e5c0917099d161
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-11 20:52:18 +0530

    Time: 1 ms (97.66%) | Memory: 42.7 MB (48.30%) - LeetSync

--- LEETCODE/258-add-digits/add-digits.java
+++ LEETCODE/258-add-digits/add-digits.java
@@ -11,3 +11,20 @@ class Solution {
         return num;
     }
 }
+
+
+/*
+class Solution {
+    public int addDigits(int num) {
+        while(num >= 10){    // As long as there is more than 1 digit in the number:
+            int sum = 0;    // Variable to store the sum
+            while(num > 0){    // As long as the number is greater than 0:
+                sum += num % 10;    // Add the last digit to the sum
+                num /= 10;    // Move to the next digit
+            }
+            num = sum;    // Store the sum in number
+        }
+        return num;    // Return the final sum of the digits of the number
+    }
+}
+*/
