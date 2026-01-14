d314ccd81957a43d46b388d6bb846d66394bda70
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-14 17:51:30 +0530

    Time: 9 ms (40.33%) | Memory: 41.9 MB (75.28%) - LeetSync

--- LEETCODE/1013-fibonacci-number/fibonacci-number.java
+++ LEETCODE/1013-fibonacci-number/fibonacci-number.java
@@ -1,20 +1,6 @@
 class Solution {
     public int fib(int n) {
-        while(n > 1){
-            return fib(n - 1) + fib (n - 2);
-        }
-        return n;
+        if(n == 0 || n == 1) return n;
+        return fib(n - 1) + fib(n - 2);
     }
 }
-
-
-/*
-class Solution {
-    public int fib(int n) {
-        while(n >= 2){    // As long as the value of n is greater than 1:
-            return fib(n - 1) + fib (n - 2);    // Recursively compute fib(n-1) + fib(n-2)
-        }
-        return n;    // If n value is less than 2, return the base value (If n=0, return 0, if n=1, return 1)
-    }
-}
-*/
