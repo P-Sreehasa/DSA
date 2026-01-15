13a014c68e51c3b282ff76141002013b6277644a
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-15 20:53:57 +0530

    Time: 9 ms (40.53%) | Memory: 42.1 MB (37.23%) - LeetSync

--- LEETCODE/1013-fibonacci-number/fibonacci-number.java
+++ LEETCODE/1013-fibonacci-number/fibonacci-number.java
@@ -4,3 +4,13 @@ class Solution {
         return fib(n - 1) + fib(n - 2);
     }
 }
+
+
+/*
+class Solution {
+    public int fib(int n) {    
+        if(n == 0 || n == 1) return n;    // If the number is 0 or 1, the fibonacci values would be 0 and 1 itself
+        return fib(n - 1) + fib(n - 2);    // For numbers greater than 1, the fibonacci value would be the sum of number's previous 2 values
+    }
+}
+*/
