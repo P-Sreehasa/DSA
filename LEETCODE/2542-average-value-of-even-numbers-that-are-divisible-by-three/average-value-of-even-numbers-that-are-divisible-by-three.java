b7eb17d6b007221a1155cc43b35c92c6571d5a6e
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-16 20:47:43 +0530

    Time: 1 ms (73.17%) | Memory: 46.2 MB (84.92%) - LeetSync

--- LEETCODE/2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.java
+++ LEETCODE/2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.java
@@ -1,36 +1,21 @@
 class Solution {
     public int averageValue(int[] nums) {
-        int s = 0;
-        int c = 0;
+        int sum = 0;
+        int count = 0;
+        int avg = 0;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] % 2 == 0 && nums[i] % 3 == 0){
-                s += nums[i];
-                c++;
+            if(nums[i] % 3 == 0){
+                int mul = nums[i];
+                if(mul % 2 == 0){
+                    sum += mul;
+                    count++;
+                }
             }
         }
-        if(c > 0){
-            return s / c;
+        if(sum > 0 && count > 0){
+            avg = sum / count;
+            return avg;
         }
         return 0;
     }
 }
-
-
-/*
-class Solution {
-    public int averageValue(int[] nums) {
-        int s = 0;    // Variable to track sum
-        int c = 0;    // Variable to track count
-        for(int i = 0; i < nums.length; i++){    // For each index in the array:
-            if(nums[i] % 2 == 0 && nums[i] % 3 == 0){    // If it is divisible by both 2 nd 3:
-                s += nums[i];    // Add it to the sum
-                c++;    // Increment the counter
-            }
-        }
-        if(c > 0){    // If the count is greater than 0:
-            return s / c;    // Return the final value by dividing sum by count
-        }
-        return 0;    // Else, return 0
-    }
-}
-*/
