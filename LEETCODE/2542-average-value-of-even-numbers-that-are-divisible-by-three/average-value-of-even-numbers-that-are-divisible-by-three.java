fd8a03145c7f291eabd0f7f29d8d081f135bc5b7
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-17 20:47:11 +0530

    Time: 1 ms (73.34%) | Memory: 46.8 MB (5.18%) - LeetSync

--- LEETCODE/2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.java
+++ LEETCODE/2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.java
@@ -19,3 +19,28 @@ class Solution {
         return 0;
     }
 }
+
+
+/*
+class Solution {
+    public int averageValue(int[] nums) {
+        int sum = 0;    // Variable to store the sum
+        int count = 0;    // Variable to count of the even numbers divisible by 3
+        int avg = 0;    // Variable to store the average of the even no's divisible by 3
+        for(int i = 0; i < nums.length; i++){    // For each index of the array:
+            if(nums[i] % 3 == 0){    // If the current value is a multiple of 3:
+                int mul = nums[i];    // Store it in a new variable
+                if(mul % 2 == 0){    // If the current value is even:
+                    sum += mul;    // Add it to the sum
+                    count++;    // Increment the counter variable
+                }
+            }
+        }
+        if(sum > 0 && count > 0){    // As long as the sum and count are not 0:
+            avg = sum / count;    // Calculate the average of the numbers
+            return avg;    // Return the average
+        }
+        return 0;    // If the sum is zero, return 0
+    }
+}
+*/
