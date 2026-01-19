f27fd212ffa696fcba29830fd740d2fbde30a861
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-19 20:14:08 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (48.29%) - LeetSync

--- LEETCODE/2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.java
+++ LEETCODE/2715-k-items-with-the-maximum-sum/k-items-with-the-maximum-sum.java
@@ -9,3 +9,18 @@ class Solution {
         return sum;
     }
 }   
+
+
+/*
+class Solution {
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int sum = 0;    // Variable to calculate the maximum possible sum
+        int ones = Math.min(k, numOnes);    // Find the no.of ones
+        sum += ones;    // Add ones to the sum
+        int zeros = Math.min(k - ones, numZeros);    // If there are zeros, add them to not change the sum
+        int negones = Math.min(k - ones - zeros, numNegOnes);    // If there are negative ones, find how many are left
+        sum -= negones;    // Subtract the negative ones from the sum
+        return sum;    // Return the final sum
+    }
+}   
+*/
