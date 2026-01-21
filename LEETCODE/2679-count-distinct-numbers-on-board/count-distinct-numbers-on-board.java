169573e06beb52d23fcdffb13509429df372d88b
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-21 21:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (80.74%) - LeetSync

--- LEETCODE/2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.java
+++ LEETCODE/2679-count-distinct-numbers-on-board/count-distinct-numbers-on-board.java
@@ -6,3 +6,15 @@ class Solution {
         return n - 1;
     }
 }
+
+
+    /*
+    class Solution {
+        public int distinctIntegers(int n) {
+            if(n == 1){    // If n is 1, return 1
+                return 1;
+            }
+            return n - 1;    // Otherwise, subtract 1 from the number and return it
+        }
+    }
+*/
