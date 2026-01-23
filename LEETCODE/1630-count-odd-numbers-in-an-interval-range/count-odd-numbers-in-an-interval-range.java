fa65eed91200285e1a0ae5f8aa5febb30e065693
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-23 21:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (97.41%) - LeetSync

--- LEETCODE/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
+++ LEETCODE/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -5,3 +5,14 @@ class Solution {
         return h - l;
     }
 }
+
+
+/*
+class Solution {
+    public int countOdds(int low, int high) {
+        int h = (high + 1) / 2;    // Count of odd numbers from 0 to high
+        int l = low / 2;    // Count of odd numbers from 0 to low - 1
+        return h - l;    // Subtract high from low to get the total count
+    }
+}
+*/
