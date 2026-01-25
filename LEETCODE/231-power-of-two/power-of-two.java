793d5abf880cbdc410c232b67eee551081e9af2d
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-25 20:34:54 +0530

    Time: 1 ms (95.64%) | Memory: 43.1 MB (11.81%) - LeetSync

--- LEETCODE/231-power-of-two/power-of-two.java
+++ LEETCODE/231-power-of-two/power-of-two.java
@@ -9,3 +9,18 @@ class Solution {
         return n == 1;
     }
 }
+
+
+/*
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0){    // Power of a number is positive
+            return false;    // If not, return false
+        } 
+        while(n % 2 == 0){    // As long as the power is divisible by 2:
+            n /= 2;    // Divide it by 2
+        }
+        return n == 1;    // Return true is it's a power of 2, otherwise false
+    }
+}
+*/
