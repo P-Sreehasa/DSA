c7518c86549f25375fbea517612972a664b1a896
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-27 23:49:12 +0530

    Time: 8 ms (85.33%) | Memory: 45.9 MB (77.76%) - LeetSync

--- LEETCODE/326-power-of-three/power-of-three.java
+++ LEETCODE/326-power-of-three/power-of-three.java
@@ -9,3 +9,18 @@ class Solution {
         return n == 1;
     }
 }
+
+
+/*
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0){    // Power should be positive
+            return false;    // If not, return false
+        }
+        while(n % 3 == 0){    // As long as the number is divisible by 3:
+            n /= 3;    // Divide it by 3
+        }
+        return n == 1;    // If the value gets reduced to 1, it's a power of 3, otherwise not
+    }
+}
+*/
