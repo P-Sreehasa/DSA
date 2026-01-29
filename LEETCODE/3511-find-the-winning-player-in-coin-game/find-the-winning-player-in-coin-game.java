9daab8e7cfbb6e68bf02cf447882439f2f41e3f4
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-29 19:02:43 +0530

    Time: 1 ms (79.03%) | Memory: 43.3 MB (65.36%) - LeetSync

--- LEETCODE/3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.java
+++ LEETCODE/3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.java
@@ -9,3 +9,18 @@ class Solution {
         return a ? "Bob" : "Alice";
     }
 }
+
+
+/*
+class Solution {
+    public String winningPlayer(int x, int y) {
+        boolean a = true;    // Tracks whose turn it is
+        while(x >= 1 && y >= 4){    // As long as each player makes a move:
+            x -= 1;    // Decrement 1 coin from value - 75
+            y -= 4;    // Decrement 4 coins from value - 10
+            a = !a;    // Give chance to the other player
+        }
+        return a ? "Bob" : "Alice";    // If a is true then Bob wins, otherwise Alice
+    }
+}
+*/
