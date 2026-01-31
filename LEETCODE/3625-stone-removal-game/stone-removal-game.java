afd96aa0b05ff6e8aaa91fae0cfdb088e4ed140c
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-01-31 12:32:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (12.21%) - LeetSync

---
+++ LEETCODE/3625-stone-removal-game/stone-removal-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canAliceWin(int n) {
+        if(n < 10){
+            return false;
+        }
+        int temp = n;
+        int t = 1;
+        int s = 10;
+        while(temp >= s){
+            temp -= s;
+            s--;
+            t = 1 - t;
+        }
+        return t == 0;
+    }
+}
