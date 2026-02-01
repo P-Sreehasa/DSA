4589307e9687ea95c6af6ee3d5fb7c55db78399c
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-02-01 17:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (32.26%) - LeetSync

--- LEETCODE/3625-stone-removal-game/stone-removal-game.java
+++ LEETCODE/3625-stone-removal-game/stone-removal-game.java
@@ -14,3 +14,23 @@ class Solution {
         return t == 0;
     }
 }
+
+
+/*
+class Solution {
+    public boolean canAliceWin(int n) {
+        if(n < 10){    // If there are less than 10 stones:
+            return false;    // Return false
+        }
+        int temp = n;    // Store n (no.of stones) in a temporary variable
+        int t = 1;    // To track whose turn it is
+        int s = 10;    // No.of stones to be removed
+        while(temp >= s){    // As long as there are greater than 's' stones:
+            temp -= s;    Remove 's' stones from temp
+            s--;    // Decrement the 's' counter
+            t = 1 - t;    // Switch the turn
+        }
+        return t == 0;    // If t is 0, Alice wins. Otherwise, Bob wins
+    }
+}
+*/
