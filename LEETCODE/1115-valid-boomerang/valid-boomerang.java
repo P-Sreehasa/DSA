4b4d79bdc0e20a1fffa7021e932c9eba6be9ff93
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-02-03 22:07:58 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (63.55%) - LeetSync

--- LEETCODE/1115-valid-boomerang/valid-boomerang.java
+++ LEETCODE/1115-valid-boomerang/valid-boomerang.java
@@ -12,3 +12,21 @@ class Solution {
         return (points[0][0] - points[1][0]) * (points[1][1] - points[2][1]) != (points[1][0] - points[2][0]) * (points[0][1] - points[1][1]);
     }
 }
+
+
+/*
+class Solution {
+    public boolean isBoomerang(int[][] points) {
+        if(points[0][0] == points[1][0] && points[0][1] == points[1][1]){    // To check if the points 0 and 1 are the same:
+            return false;    // If yes, then it's not a boomerang
+        }
+        if(points[0][0] == points[2][0] && points[0][1] == points[2][1]){   // To check if the points 0 and 2 are the same:
+            return false;    // If yes, then it's not a boomerang
+        }
+        if(points[1][0] == points[2][0] && points[1][1] == points[2][1]){   // To check if the points 1 and 2 are the same:
+            return false;    // If yes, then it's not a boomerang
+        }
+        return (points[0][0] - points[1][0]) * (points[1][1] - points[2][1]) != (points[1][0] - points[2][0]) * (points[0][1] - points[1][1]);    // If area is not 0, then the points aren't on the same line
+    }
+}
+*/
