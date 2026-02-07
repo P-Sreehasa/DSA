69bd414d2baf68625d3b43ba73106b01c3a444f7
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-02-07 20:17:00 +0530

    Time: 16 ms (96.54%) | Memory: 49.9 MB (30.69%) - LeetSync

--- LEETCODE/561-array-partition/array-partition.java
+++ LEETCODE/561-array-partition/array-partition.java
@@ -8,3 +8,17 @@ class Solution {
         return sum;
     }
 }
+
+
+/*
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);    // Sort the array
+        int sum = 0;    // Variable to calculate the sum of array elements
+        for(int i = 0; i < nums.length; i += 2){    // For every 2nd element of the array:
+            sum += nums[i];    // Add it to the sum
+        }
+        return sum;    // Return the sum of the array elements
+    }
+}
+*/
