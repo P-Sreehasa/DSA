409c85d6dd87feebf61ebd4517e00661609fd469
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-02-08 22:27:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (68.04%) - LeetSync

--- LEETCODE/292-nim-game/nim-game.java
+++ LEETCODE/292-nim-game/nim-game.java
@@ -4,11 +4,3 @@ class Solution {
     }
 }
 
-
-/*
-class Solution {
-    public boolean canWinNim(int n) {
-        return n % 4 != 0;    // It's a win if and only if n is not a multiple of 4
-    }
-}
-*/
