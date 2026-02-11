c8b345ec6406ed67056bd7645bf9ecc8fc355f6f
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-02-11 20:34:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (27.77%) - LeetSync

--- LEETCODE/1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
+++ LEETCODE/1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.java
@@ -13,20 +13,3 @@ class Solution {
     }
 }
 
-
-/*
-class Solution {
-    public int[] sumZero(int n) {
-        int res[] = new int[n];    // Array to store the result
-        int c = 0;    // Variable to track the array index for storing the values
-        for(int i = 1; i <= n / 2; i++){    // For each index in the range of the array:
-            res[c++] = -i;    // Add the negative number to the array
-            res[c++] = i;    // Add the positive number to the array (since, negative and positive sum up to 0)
-        } 
-        if(n % 2 != 0){    // If n is odd:
-            res[c] = 0;    // Add 0 to maintain the correct count and the value of sum as 0
-        }
-        return res;    // Return the final array 
-    }
-}
-*/
