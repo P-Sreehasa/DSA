57f27d4fe8b25ab2623311ac21cc70bff9a19250
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-03-08 19:14:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (73.82%) - LeetSync

--- LEETCODE/1791-richest-customer-wealth/richest-customer-wealth.java
+++ LEETCODE/1791-richest-customer-wealth/richest-customer-wealth.java
@@ -13,22 +13,3 @@ class Solution {
         return w;
     }
 }
-
-
-/*
-class Solution {
-    public int maximumWealth(int[][] accounts) {
-        int w = 0;    // Variable to store the maximum wealth
-        for(int i = 0; i < accounts.length; i++){    // For each value in the array:
-            int m = 0;    // Variable to store the current maximum
-            for(int j = 0; j < accounts[i].length; j++){    // For each value in the array:
-                m += accounts[i][j];    // Add it to the maximum variable
-            }
-            if(m > w){    // If the current maximum is greater than the maximum wealth:
-                w = m;    // Store the maximum in m
-            }
-        }
-        return w;    // Return the final maximum
-    }
-}
-*/
