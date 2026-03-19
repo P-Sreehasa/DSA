0e5535bd9c2ac70c10b31742551686ce609b6f58
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-03-19 20:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (90.14%) - LeetSync

--- LEETCODE/2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.java
+++ LEETCODE/2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.java
@@ -3,12 +3,3 @@ class Solution {
         return(arrivalTime + delayedTime) % 24;
     }
 }
-
-
-/*
-class Solution {
-    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
-        return(arrivalTime + delayedTime) % 24;    // As it's in 24-hours format, return the remainder as the final arrival time
-    }
-}
-*/
