ca757f61abc89995cfa9b22ead74dcb2859d3836
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-03-24 20:25:26 +0530

    Time: 4 ms (88.41%) | Memory: 42.2 MB (90.26%) - LeetSync

--- LEETCODE/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ LEETCODE/767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -20,29 +20,3 @@ class Solution {
         return c;
     }
 }
-
-
-/*
-class Solution {
-    public int countPrimeSetBits(int left, int right) {
-        int c = 0;    // Variable to track the count of prime no.of set-bits
-        for(int i = left; i <= right; i++){    // For each value in the range of left and right:
-            int sb = Integer.bitCount(i);    // Count the set-bits of i
-            if(sb < 2){    // If the set-bits values is less than 2:
-                continue;    // Skip them
-            }
-            boolean p = true;    // Flag bit to track if the set-bits are prime
-            for(int j = 2; j * j <= sb; j++){    // For each value:
-                if(sb % j == 0){    // If the set-bit is divisible by j:
-                    p = false;    // Set the flag bit to false
-                    break;    // Terminate the condition
-                }
-            }
-            if(p){    // If the value is prime:
-                c++;    // Increment the counter variable
-            }
-        }
-        return c;    // Return the final count
-    }
-}
-*/
