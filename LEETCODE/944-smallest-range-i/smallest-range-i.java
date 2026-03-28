051d1b0cfb3b02e057ef1de70b683affebecc927
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-03-28 18:12:19 +0530

    Time: 3 ms (60.75%) | Memory: 47.1 MB (86.18%) - LeetSync

--- LEETCODE/944-smallest-range-i/smallest-range-i.java
+++ LEETCODE/944-smallest-range-i/smallest-range-i.java
@@ -12,20 +12,3 @@ class Solution {
     }
 }
 
-
-/*
-class Solution {
-    public int smallestRangeI(int[] nums, int k) {
-        int n = nums.length;    // Calculate the size of the array
-        int minimum = Integer.MAX_VALUE;    // Assign the maximum value to the minimum variable
-        int maximum = Integer.MIN_VALUE;    // Assign the minimum value to the maximum variable
-        for(int i = 0; i < n; i++){    // For each index of the array:
-            minimum = Math.min(minimum, nums[i]);    // Calculate the minimum value 
-            maximum = Math.max(maximum, nums[i]);    // Calculate the maximum value 
-        }
-        int score = Math.max(0, maximum - minimum - 2 * k);    // The score is calculated by subtracting twice k from the difference between maximum and minimum
-
-        return score;    // Return the final score
-    }
-}
-*/
