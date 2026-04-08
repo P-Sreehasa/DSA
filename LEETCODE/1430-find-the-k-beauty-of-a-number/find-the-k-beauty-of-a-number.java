b5f421d014db9fe2a3e991c0d8237120016e453f
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-04-08 18:51:53 +0530

    Time: 1 ms (93.37%) | Memory: 42.2 MB (79.21%) - LeetSync

--- LEETCODE/1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.java
+++ LEETCODE/1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.java
@@ -16,3 +16,23 @@ class Solution {
     }
 }
 
+
+/*
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        String s = String.valueOf(num);    // Convert the integer to string
+        int c = 0;    // Variable to track count 
+        for(int i = 0; i <= s.length() - k; i++){    // For each string of 'K' length:
+            String ss = s.substring(i, i + k);    // Extract sub-string of 'K' length from i 
+            int n = Integer.parseInt(ss);    // Convert the string back to integer
+            if(n == 0){    // If sub-string is 0:
+                continue;    // Skip it
+            }
+            if(num % n == 0){    // If the number is divisible by the sub-string:
+                c++;    // Increment the counter variable
+            }
+        }
+        return c;    // Return the final count
+    }
+}
+*/
