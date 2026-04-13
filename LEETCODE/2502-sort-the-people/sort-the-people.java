17e4569d9bc961ee2a12d76bc35c66c81d759657
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-04-13 21:59:58 +0530

    Time: 8 ms (93.03%) | Memory: 47.3 MB (54.97%) - LeetSync

--- LEETCODE/2502-sort-the-people/sort-the-people.java
+++ LEETCODE/2502-sort-the-people/sort-the-people.java
@@ -12,21 +12,3 @@ class Solution {
         return arr;
     }
 }
-
-
-/*
-class Solution {
-    public String[] sortPeople(String[] names, int[] heights) {
-        Integer n[] = new Integer[names.length];    // Array to store the indices of people
-        for(int i = 0; i < names.length; i++){    // For each index of the array:
-            n[i] = i;    // Store it into the array
-        }
-        Arrays.sort(n, (a, b) -> heights[b] - heights[a]);    // Sort the array in descending order 
-        String arr[] = new String[names.length];    // Array to store the result
-        for(int i = 0; i < names.length; i++){    // For each index of the array:
-            arr[i] = names[n[i]];    // From the sorted array, store them into the result array
-        }
-        return arr;    // Return the final array
-    }
-}
-*/
