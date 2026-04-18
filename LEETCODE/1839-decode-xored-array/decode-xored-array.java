f00ec134dfbfc9357e14bd76e8c46ee3fdbb7ede
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-04-18 21:23:17 +0530

    Time: 2 ms (96.69%) | Memory: 47.7 MB (86.15%) - LeetSync

--- LEETCODE/1839-decode-xored-array/decode-xored-array.java
+++ LEETCODE/1839-decode-xored-array/decode-xored-array.java
@@ -9,3 +9,18 @@ class Solution {
         return arr;
     }
 }
+
+
+/*
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int n = encoded.length;    // Variable to store the length of the array
+        int arr[] = new int[n + 1];    // Create a new array with size 1 more than the encoded array
+        arr[0] = first;    // Store the first element at the starting index
+        for(int i = 0; i < n; i++){    // For each index of the encoded array:
+            arr[i + 1] = arr[i] ^ encoded[i];    // Store the XORed result in the next index
+        }
+        return arr;    // Return the final array
+    }
+}
+*/
