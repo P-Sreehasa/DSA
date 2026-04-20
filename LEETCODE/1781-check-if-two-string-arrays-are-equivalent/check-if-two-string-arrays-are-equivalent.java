03ba126297f7c6a220856aebedcc7abe845c227e
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-04-20 20:31:08 +0530

    Time: 3 ms (8.80%) | Memory: 43.2 MB (75.42%) - LeetSync

--- LEETCODE/1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ LEETCODE/1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -20,3 +20,29 @@ class Solution {
         return i == word1.length && j == word2.length;
     }
 }
+
+
+/*
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        int p1 = 0, p2 = 0;    // Indices for the strings in the 2 arrays
+        int i = 0, j= 0;    // Indices for the arrays
+        while(i < word1.length && j < word2.length){    // While i and j are in the range of the indices:
+            if(word1[i].charAt(p1) != word2[j].charAt(p2)){    // Compare the current characters of both the arrays:
+                return false;    // If they're different, strings aren't equal
+            }
+            p1++;    // Move to the next character of word1
+            if(p1 == word1[i].length()){    // If the first string ends:
+                i++;    // Move to the next
+                p1 = 0;    // Reset the first string 
+            }
+            p2++;    // Move to the next character of word2
+            if(p2 == word2[j].length()){    // If the second string ends:
+                j++;    // Move to the next
+                p2 = 0;    // Reset the second string  
+            }
+        }
+        return i == word1.length && j == word2.length;    // Return the result by comparing both the arrays
+    }
+}
+*/
