8442f6531ae76fe626e4f96723285d0de23b7e94
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-04-28 21:03:47 +0530

    Time: 6 ms (31.33%) | Memory: 46.6 MB (95.87%) - LeetSync

--- LEETCODE/3226-minimum-number-game/minimum-number-game.java
+++ LEETCODE/3226-minimum-number-game/minimum-number-game.java
@@ -13,3 +13,22 @@ class Solution {
         return arr;
     }
 }
+
+
+/*
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);    // Sort the array
+        int n = nums.length;    // Calculate the length of the array
+        int arr[] = new int[n];    // New array to store the result
+        int t = 0;    // Variable used for inserting values into the array
+        for(int i = 0; i < n; i = i + 2){    // For each value-pair in the array:
+            int a = nums[i];    // Alice takes the smallest
+            int b = nums[i + 1];    // Bob takes the next smallest
+            arr[t++] = b;    // Bob adds first
+            arr[t++] = a;    // Alice adds next
+        }
+        return arr;    // Return the final array
+    }
+}
+*/
