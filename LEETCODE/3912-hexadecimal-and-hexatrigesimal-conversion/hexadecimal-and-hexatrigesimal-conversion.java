b40cd3d1dcdcfdc5880458d5a88346c95a23b75c
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-05-05 18:23:04 +0530

    Time: 3 ms (87.65%) | Memory: 43.8 MB (58.64%) - LeetSync

--- LEETCODE/3912-hexadecimal-and-hexatrigesimal-conversion/hexadecimal-and-hexatrigesimal-conversion.java
+++ LEETCODE/3912-hexadecimal-and-hexatrigesimal-conversion/hexadecimal-and-hexatrigesimal-conversion.java
@@ -2,7 +2,7 @@ class Solution {
     public String concatHex36(int n) {
         int n1 = n * n;
         int n2 = n1 * n;
-        String h = Integer. toHexString(n1).toUpperCase();
+        String h = Integer.toHexString(n1).toUpperCase();
         String b = Integer.toString(n2, 36).toUpperCase();
         return h + b;
     }
