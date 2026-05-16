82589e821e60ac1b86000c9c6696a29af6906653
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-05-16 11:00:26 +0530

    Time: 1 ms (99.61%) | Memory: 44.4 MB (77.97%) - LeetSync

--- LEETCODE/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ LEETCODE/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -16,3 +16,23 @@ class Solution {
     }
 }
 
+
+/*
+class Solution {
+    public int findNumbers(int[] nums) {
+        int c = 0;    // Variable to track count of even digits in the numbers
+        for(int n : nums){    // For each value in the array:
+            int d = 0;    // Variable to track the count of digits in each number
+            int t = n;    // Store the number in a temporary variable
+            while(t != 0){    // As long as the temporary number is not zero:
+                t /= 10;    // Remove the last digit
+                d++;    // Increment digit variable's count
+            }
+            if(d % 2 == 0){    // If the count of digits is even:
+                c++;    // Increment the counter variable's value
+            }
+        }
+        return c;    // Return the final count
+    }
+}
+*/
