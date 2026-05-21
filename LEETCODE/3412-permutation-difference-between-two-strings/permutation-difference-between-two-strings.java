a4cfd9034b83aca1ff9fbb502dc30ac5f88e056e
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-05-21 21:09:26 +0530

    Permutation-difference-between-two-strings.java

--- LEETCODE/3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.java
+++ LEETCODE/3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.java
@@ -2,12 +2,12 @@ class Solution {
     public int findPermutationDifference(String s, String t) {
         int pos[] = new int[26];
         int len_s = s.length();
-        for(int i = 0; i < len_s; i++) {
+        for(int i = 0; i < len_s; i++){
             pos[s.charAt(i) - 'a'] = i;
         }
         int diff = 0;
         int len_t = s.length();
-        for(int i = 0; i < len_t; i++) {
+        for(int i = 0; i < len_t; i++){
             diff += Math.abs(i - pos[t.charAt(i) - 'a']);
         }
         return diff;
