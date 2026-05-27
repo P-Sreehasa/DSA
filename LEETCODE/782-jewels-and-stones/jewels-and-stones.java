5e1ff2524a20105377e08e510afdce318758a259
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-05-27 21:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (35.06%) - LeetSync

---
+++ LEETCODE/782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int arr[]= new int[128]; 
+        for(char a : jewels.toCharArray()) {
+            arr[a] = 1;
+        }
+        int c = 0;
+        for(char a : stones.toCharArray()) {
+            if (arr[a] == 1) {
+                c++;
+            }
+        }
+        return c;
+    }
+}
