40e88b30b12de79efd062956f9b028c3ca5941e5
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-06-19 20:54:34 +0530

    Time: 33 ms (41.01%) | Memory: 46.5 MB (43.63%) - LeetSync

--- LEETCODE/2998-count-symmetric-integers/count-symmetric-integers.java
+++ LEETCODE/2998-count-symmetric-integers/count-symmetric-integers.java
@@ -19,30 +19,3 @@ class Solution {
         return c;
     }
 }
-
-
-/*
-class Solution {
-    public int countSymmetricIntegers(int low, int high) {
-        int c = 0;    // Variable to track count of symmetric integers
-        for(int i = low; i <= high; i++){    // For every value in the range of low and high:
-            String str = String.valueOf(i);    // Convert the integer to string
-            int l = str.length();    // Calculate the no.of digits for it
-            if(l % 2 != 0){    // If the no.of digits in the value is odd:
-                continue;    // Skip it
-            }
-            int m = l / 2;    // Divide the value into 2 parts
-            int s = 0;    // Variable for storing the difference between the sum of first and second parts
-            for(int j = 0; j < l; j++){    // For every value in the range of the length:
-                s += (str.charAt(j) - '0') * (j < m ? 1 : -1);    // Get the numeric value of the digit at that position and:
-                if the digit in in the first part -> add it to sum
-                else -> subtract it from sum
-            }
-            if(s == 0){    // If the digit sum is 0: 
-                c++;    // Increment the count value
-            }
-        }
-        return c;    // Return the final count
-    }
-}
-*/
