60e2ef6ef3e22ee244908762fbf1af8efc905bc5
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-07-17 22:41:12 +0530

    smallest-even-multiple.java

--- LEETCODE/2491-smallest-even-multiple/smallest-even-multiple.java
+++ LEETCODE/2491-smallest-even-multiple/smallest-even-multiple.java
@@ -1,8 +1,7 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        if(n % 2 != 0){
+        if(n % 2 != 0)
             return n * 2;
-        }
         return n;
     }
 }
@@ -11,9 +10,8 @@ class Solution {
 /*
 class Solution {
     public int smallestEvenMultiple(int n) {
-        if(n % 2 != 0){    // If n is Odd:
+        if(n % 2 != 0)    // If n is Odd:
                 return n * 2;    // Multiply it by 2
-        }
         return n;    // If n is Even, return that number itself
     }
 }
