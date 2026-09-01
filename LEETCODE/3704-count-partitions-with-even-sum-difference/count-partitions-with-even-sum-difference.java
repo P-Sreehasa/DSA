d740dfd472165e7d9043ce0595491d1d6480f811
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-09-01 21:47:19 +0530

    Time: 1 ms (54.86%) | Memory: 43.3 MB (96.84%) - LeetSync

--- LEETCODE/3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ LEETCODE/3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,43 +1,19 @@
 class Solution {
     public int countPartitions(int[] nums) {
-        //int n = nums.length;
-        int s = 0;
-        for(int n : nums){
-            s += n;
-        }
-        int c = 0;
-        int l = 0;
+        int count = 0;
+        int left_part = 0;
         for(int i = 0; i < nums.length - 1; i++){
-            l += nums[i];
-            int r = s - l;
-            int d = l - r;
-            if(d % 2 == 0){
-                c++;
+            int current = nums[i];
+            left_part += current;
+            int right_part = 0;
+            for(int j = i + 1; j < nums.length; j++){
+                right_part += nums[j];
+            }
+            int res = left_part - right_part;
+            if(res % 2 == 0){
+                count++;
             }
         }
-        return c;
+        return count;
     }
 }
-
-
-/*
-class Solution {
-    public int countPartitions(int[] nums) {
-        int s = 0;    // Variable to track sum
-        for(int n : nums){    // For each value in the array:
-            s += n;    // Add it to the sum
-        }
-        int c = 0;    // Variable to track valid partitions 
-        int l = 0;    // Variable to track the left partition's sum
-        for(int i = 0; i < nums.length - 1; i++){    // For each value in the partition:
-            l += nums[i];    // Add the current index t the left partition
-            int r = s - l;    // Variable to track the right partition's sum by subtracting sum from left partition's sum
-            int d = l - r;    // Calculate the difference between the left and right partitions
-            if(d % 2 == 0){    // If the difference is even:
-                c++;    // Increment the counter variable
-            }
-        }
-        return c;    // Return the final count
-    }
-}
-*/
