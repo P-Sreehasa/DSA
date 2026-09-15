26b517ee1fee627667db43b40e4229ebdffc134e
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-09-15 22:08:24 +0530

    Time: 2105 ms (26.45%) | Memory: 41.9 MB (79.51%) - LeetSync

--- LEETCODE/507-perfect-number/perfect-number.java
+++ LEETCODE/507-perfect-number/perfect-number.java
@@ -1,38 +1,14 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        if(num <= 1){
-            return false;
-        }
-        int s = 1;
-        for(int i = 2; i * i <= num; i++){
+        int sum = 0;
+        for(int i = 1; i < num; i++){
             if(num % i == 0){
-                s += i;
-                if(i != num / i){
-                    s += num / i;
-                }
+                sum += i;
             }
         }
-        return s == num;
+        if(sum == num){
+            return true;
+        }
+        return false;
     }
 }
-
-
-/*
-class Solution {
-    public boolean checkPerfectNumber(int num) {
-        if(num <= 1){    // If number is less than 1:
-            return false;    // Return false, (Because perfect squares start from 1)
-        }
-        int s = 1;    // Variable to calculate the sum
-        for(int i = 2; i * i <= num; i++){    // For each value from 2 to square-root of the number:
-            if(num % i == 0){    // If it is perfectly divisible by the number:
-                s += i;    // Add it to the sum
-                if(i != num / i){    // If the current value is not the square-root:
-                    s += num / i;    // Add the other factor to the sum
-                }
-            }
-        }
-        return s == num;    // Return the final value
-    }
-}
-*/
