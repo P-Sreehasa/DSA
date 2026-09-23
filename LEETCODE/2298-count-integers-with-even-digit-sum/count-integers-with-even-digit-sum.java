4d9708fa2ee011ba56b928da805c9f2aaacbc8af
Sreehasa <193766074+P-Sreehasa@users.noreply.github.com>
2026-09-23 22:41:32 +0530

    Time: 2 ms (63.00%) | Memory: 42.2 MB (40.47%) - LeetSync

--- LEETCODE/2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.java
+++ LEETCODE/2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.java
@@ -1,38 +1,18 @@
 class Solution {
     public int countEven(int num) {
-        int c = 0;
-        for(int i = 1; i <= num; i++){
-            int s = 0;
-            int l = i;
-            while(l > 0){
-                s += l % 10;
-                l /= 10;
+        int count = 0;
+        for(int i = 2; i <= num; i++){
+            int sum = 0;
+            int n = i;
+            while(n > 0){
+                int rem = n % 10;
+                sum += rem;
+                n /= 10;
             }
-            if(s % 2 == 0){
-                c++;
-            }
-        }
-        return c;
-    }
-};
-
-
-/*
-class Solution {
-    public int countEven(int num) {
-        int c = 0;    // Counter variable to track no.of even-summed digits
-        for(int i = 1; i <= num; i++){    // For each value in the range of 1 and n:
-            int s = 0;    // Variable to track sum of digits
-            int l = i;    // Store the current value in the last-digit variable
-            while(l > 0){    // As long as the last-digit is greater than 0:
-                s += l % 10;    // Get the last digit and add it to sum
-                l /= 10;    // Remove the last digit
-            }
-            if(s % 2 == 0){    // If the sum is even:
-                c++;    // Increment the counter variable
+            if(sum % 2 == 0){
+                count++;
             }
         }
-        return c;    // Return the final count
+        return count;
     }
-};
-*/
+}
